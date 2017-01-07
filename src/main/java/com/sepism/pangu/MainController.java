11f5d2502a60e863531cffcf6bcb5edaa22b6604
zhshuai1 <zh_ang_ok@yeah.net>
2017-01-07 18:07:08 +0800

    add .gitignore and remove target from repository

--- src/main/java/com/sepism/pangu/MainController.java
+++ src/main/java/com/sepism/pangu/MainController.java
@@ -1,32 +1,33 @@
 package com.sepism.pangu;
 
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import lombok.Setter;
 
-import java.util.Map;
 
+@Setter
 @Controller
 @SessionAttributes("merchantId")
+@Log4j2
 public class MainController {
 
     @RequestMapping(value = "/say", method = RequestMethod.GET)
     public String sayHello2(@RequestParam("name") String id, Model model) {
-        System.out.println(id + "2");
+        log.info("The value of name is {}\n\n\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++" +
+                "++++++++++++++",id);
         model.addAttribute("url", "world");
         return "test";
     }
+}
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Map<String, String> model) {
-        System.out.println("hello");
-        model.put("url", "456");
-        return "index";
-    }
-
-    @RequestMapping(value = "/sessionTest", method = RequestMethod.GET)
-    public String sessionTest(@ModelAttribute("merchantId") String merchantId) {
-        System.out.println(merchantId);
-        return "sessionTest";
-    }
+@Getter
+@Setter
+class Hello {
+    private int hello;
 }
