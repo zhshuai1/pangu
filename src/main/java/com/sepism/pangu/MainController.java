1bd1750ebb0c31e01c0c4eb2ef0a8e66fead31c7
zhshuai1 <zh_ang_ok@yeah.net>
2017-04-01 08:26:07 +0800

    config log4j2

--- src/main/java/com/sepism/pangu/MainController.java
+++ src/main/java/com/sepism/pangu/MainController.java
@@ -1,14 +1,18 @@
 package com.sepism.pangu;
 
-import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.EventLogger;
+import org.apache.logging.log4j.ThreadContext;
+import org.apache.logging.log4j.message.StructuredDataMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import lombok.Setter;
+
+import java.util.UUID;
 
 
 @Setter
@@ -19,14 +23,22 @@ public class MainController {
 
     @RequestMapping(value = "/say", method = RequestMethod.GET)
     public String sayHello2(@RequestParam("name") String id, Model model) {
-        log.info("The value of name is {}\n\n\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++" +
-                "++++++++++++++",id);
+        ThreadContext.push(UUID.randomUUID().toString());
+        log.error("The value of name is {}", id);
         model.addAttribute("url", "world");
+        myTest("Aloha!!!");
+        ThreadContext.clearAll();
         return "test";
     }
 
+    private void myTest(String testStr) {
+        log.trace(testStr);
+        EventLogger.logEvent(new StructuredDataMessage(UUID.randomUUID().toString().replaceAll("\\-", ""), testStr, "TYPE"));
+        log.debug("This is a debug message");
+    }
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login( Model model) {
+    public String login(Model model) {
         return "login";
     }
 }
