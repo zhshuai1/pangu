11de9eaf2f52e13c2a50488ff5e95c4410dba597
zhshuai1 <zh_ang_ok@yeah.net>
2017-04-12 07:55:50 +0800

    config hibernate for Choice.java and Question.java

--- src/main/java/com/sepism/pangu/model/question/Choice.java
+++ src/main/java/com/sepism/pangu/model/question/Choice.java
@@ -3,10 +3,14 @@ package com.sepism.pangu.model.question;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.Date;
 
 @Getter
 @Setter
+@Entity
+@Table(name="choices")
 public class Choice {
     private long id;
     private long questionId;
--- src/main/java/com/sepism/pangu/model/question/Question.java
+++ src/main/java/com/sepism/pangu/model/question/Question.java
@@ -3,10 +3,14 @@ package com.sepism.pangu.model.question;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.Date;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "questions")
 public class Question {
     private long id;
     private String title;
