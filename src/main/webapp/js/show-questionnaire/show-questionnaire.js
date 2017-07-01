e24c99b951d1e37897786bb56c32929c1fb2b343
zhshuai1 <zh_ang_ok@yeah.net>
2017-07-01 16:03:59 +0800

    use annotation to guarantee minify safe

--- src/main/webapp/js/show-questionnaire/show-questionnaire.js
+++ src/main/webapp/js/show-questionnaire/show-questionnaire.js
@@ -1,12 +1,12 @@
 var questionnaireApp = angular.module('questionnaireApp', []);
 questionnaireApp.component("questionnaire", {
     templateUrl: "template/questionnaire.template.html",
-    controller: function QuestionnaireController($scope, $http) {
+    controller: ['$http', function ($http) {
         var self = this;
         $http.get('/questionnaires/2').then(function (response) {
             console.log(response.data.questions);
             self.questions = response.data.questions;
         });
-    },
+    }],
 });
 
