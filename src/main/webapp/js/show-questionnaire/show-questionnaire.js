b1a08643c03a4d43d111ad6428813917683b2dc0
zhshuai1 <zh_ang_ok@yeah.net>
2017-07-01 15:34:45 +0800

    introduce  to angular

--- src/main/webapp/js/show-questionnaire/show-questionnaire.js
+++ src/main/webapp/js/show-questionnaire/show-questionnaire.js
@@ -1,26 +1,12 @@
 var questionnaireApp = angular.module('questionnaireApp', []);
 questionnaireApp.component("questionnaire", {
-    templateUrl:"template/questionnaire.template.html" ,
-    controller: function QuestionnaireController($scope) {
-        this.questions = [
-            {
-                title: "How old are you?",
-                type: "TEXT",
-
-            },
-            {
-                title: "What is your gender?",
-                type: "RADIO"
-            },
-            {
-                title: "What sports do you like?",
-                type: "CHECKBOX"
-            },
-            {
-                title: "When were you born?",
-                type: "DATE"
-            },
-        ];
+    templateUrl: "template/questionnaire.template.html",
+    controller: function QuestionnaireController($scope, $http) {
+        var self = this;
+        $http.get('/questionnaires/2').then(function (response) {
+            console.log(response.data.questions);
+            self.questions = response.data.questions;
+        });
     },
 });
 
