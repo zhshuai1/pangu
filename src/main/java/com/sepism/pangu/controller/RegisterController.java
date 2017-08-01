8e210f9686ba31c04b523a4e4d83e64733648e39
zhshuai1 <zh_ang_ok@yeah.net>
2017-08-01 11:05:18 +0800

    add placeholder controller to handle complete-info post request

--- src/main/java/com/sepism/pangu/controller/RegisterController.java
+++ src/main/java/com/sepism/pangu/controller/RegisterController.java
@@ -1,7 +1,9 @@
 package com.sepism.pangu.controller;
 
 import com.google.gson.Gson;
+import com.sepism.pangu.constant.ErrorCode;
 import com.sepism.pangu.handler.SepHandler;
+import com.sepism.pangu.model.handler.Response;
 import com.sepism.pangu.model.register.RegisterRequest;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +18,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class RegisterController {
 
+    private static final Gson GSON = new Gson();
     @Autowired
     private SepHandler registerHandler;
-
-    private static final Gson GSON = new Gson();
 
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String getRegisterPage() {
@@ -34,6 +35,15 @@ public class RegisterController {
         log.info("User is registering with information: " + formData);
         RegisterRequest request = GSON.fromJson(formData, RegisterRequest.class);
         return registerHandler.handle(request).serialize();
+    }
+
+    @RequestMapping(path = "/complete-info", method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public String completeInfo(@RequestBody String formData) {
+        log.info("User is completing with information: " + formData);
+        return new Response(ErrorCode.SUCCESS).serialize();
     }
 
 }
