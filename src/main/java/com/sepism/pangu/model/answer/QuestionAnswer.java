6570620da86fd577b4394dee1d10124c1de01b01
zhshuai1 <zh_ang_ok@yeah.net>
2017-09-19 23:18:56 +0800

    add dao for questionAnswer and questionnaireAnswers

--- src/main/java/com/sepism/pangu/model/answer/QuestionAnswer.java
+++ src/main/java/com/sepism/pangu/model/answer/QuestionAnswer.java
@@ -1,17 +1,26 @@
 package com.sepism.pangu.model.answer;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Getter
 @Setter
 @ToString
+@Builder
+@Entity
+@Table(name = "questionAnswers")
 public class QuestionAnswer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private long questionId;
+    private long questionnaireId;
+    private long questionnaireAnswerId;
     private long userId;
     private String answer;
     private Date creationDate;
--- src/main/java/com/sepism/pangu/model/answer/QuestionnaireAnswer.java
+++ src/main/java/com/sepism/pangu/model/answer/QuestionnaireAnswer.java
@@ -1,17 +1,24 @@
 package com.sepism.pangu.model.answer;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-import javax.persistence.Id;
-import java.util.List;
+import javax.persistence.*;
+import java.util.Date;
 
 @Getter
 @Setter
+@ToString
+@Builder
+@Entity
+@Table(name = "questionnaireAnswers")
 public class QuestionnaireAnswer {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private long userId;
     private long questionnaireId;
-    private List<QuestionAnswer> questionAnswers;
-
+    private Date creationDate;
 }
