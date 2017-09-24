eee2d21fa3590322599ad1099a37a96ab214d081
zhshuai1 <zh_ang_ok@yeah.net>
2017-09-24 16:22:48 +0800

    add one vivid log in health check controller

--- src/main/java/com/sepism/pangu/controller/HealthCheckController.java
+++ src/main/java/com/sepism/pangu/controller/HealthCheckController.java
@@ -1,15 +1,18 @@
 package com.sepism.pangu.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Log4j2
 @Controller
 public class HealthCheckController {
     @RequestMapping(path = "/ping", method = RequestMethod.GET)
     @ResponseBody
     public String ping() {
+        log.info("I'm happy. Because this is my character setting.");
         return "Sepism is healthy.";
     }
 }
