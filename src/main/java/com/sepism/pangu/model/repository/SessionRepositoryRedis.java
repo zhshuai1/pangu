60c8f0152150c0f960dd47baf3946ee6df4ef35c
zhshuai1 <zh_ang_ok@yeah.net>
2017-10-13 09:09:08 +0800

    Generate Jedis client on demand
    It seems that Jedis is not thread-safe. For a long term solution, will configure a connection pool to improve the performance.

--- src/main/java/com/sepism/pangu/model/repository/SessionRepositoryRedis.java
+++ src/main/java/com/sepism/pangu/model/repository/SessionRepositoryRedis.java
@@ -2,25 +2,29 @@ package com.sepism.pangu.model.repository;
 
 import com.sepism.pangu.constant.GlobalConstant;
 import com.sepism.pangu.model.authentication.Session;
+import com.sepism.pangu.util.Configuration;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Transaction;
 
 import java.util.List;
 
+/**
+ * TODO: It seems that jedis is not thread-safe. For workaround, will new Jedis when needed. Should configure a
+ * connection pool to solve this problem.
+ */
 @Component
 @Log4j2
 public class SessionRepositoryRedis {
     private static final String SESSION_PREFIX = "session:";
     private static final String TOKEN = "token";
-    @Autowired
-    private Jedis jedis;
+    private static final String REDIS_HOST = Configuration.get("redisHost");
 
     public Session findOne(long userId) {
-        List<String> elements = jedis.hmget(SESSION_PREFIX + userId, TOKEN);
-        return Session.builder().id(userId).token(elements.get(0)).build();
+        Jedis jedis = new Jedis(REDIS_HOST);
+        String token = jedis.hget(composeKey(userId), TOKEN);
+        return Session.builder().id(userId).token(token).build();
     }
 
     public void save(Session session) {
@@ -28,16 +32,21 @@ public class SessionRepositoryRedis {
             log.error("The session to be saved is null. This is very strange.");
             return;
         }
-        // Here should use watch to avoid other client to change the key during the operation. But considering the
-        // chance is quite low, and there is not much impact, I will ignore the watch.
+        Jedis jedis = new Jedis(REDIS_HOST);
+        String key = composeKey(session.getId());
+        jedis.watch(key);
         Transaction transaction = jedis.multi();
-        jedis.hset(SESSION_PREFIX + session.getId(), TOKEN, session.getToken());
-        jedis.pexpire(SESSION_PREFIX + session.getId(), GlobalConstant.SESSION_EXPIRED_TIME);
+        transaction.hset(key, TOKEN, session.getToken());
+        transaction.pexpire(key, GlobalConstant.SESSION_EXPIRED_TIME);
         List<Object> result = transaction.exec();
         if (result == null) {
             log.error("Failed to exec the transaction when storing session to redis.");
         } else {
             log.info("The transaction exec result is: {}", result);
         }
+    }
+
+    private String composeKey(long id) {
+        return SESSION_PREFIX + id;
     }
 }
