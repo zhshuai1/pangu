5714bceedd05c886c10513f2f3d0ca2528d53e5d
zhshuai1 <zh_ang_ok@yeah.net>
2017-10-14 20:44:55 +0800

    fix wrong import issue

--- src/main/java/com/sepism/pangu/model/repository/QuestionnaireAnswerRepository.java
+++ src/main/java/com/sepism/pangu/model/repository/QuestionnaireAnswerRepository.java
@@ -2,11 +2,11 @@ package com.sepism.pangu.model.repository;
 
 import com.sepism.pangu.model.answer.QuestionnaireAnswer;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Transactional(readOnly = true)
