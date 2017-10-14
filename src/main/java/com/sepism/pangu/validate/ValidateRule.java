1650021a4307156e43df36589bdb35aad195b5ab
zhshuai1 <zh_ang_ok@yeah.net>
2017-10-14 20:55:51 +0800

    remove unused classes

--- src/main/java/com/sepism/pangu/validate/ValidateRule.java
+++
@@ -1,4 +0,0 @@
-package com.sepism.pangu.validate;
-
-public class ValidateRule {
-}
--- src/main/java/com/sepism/pangu/validate/Validator.java
+++
@@ -1,5 +0,0 @@
-package com.sepism.pangu.validate;
-
-public interface Validator {
-    boolean validate(String value, ValidateRule validateRule);
-}
