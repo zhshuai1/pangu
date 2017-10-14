12487a9c39cfef2513e548c065146c673989ab14
zhshuai1 <zh_ang_ok@yeah.net>
2017-10-14 10:03:58 +0800

    add pageable api to allow gather full quantity data from db

--- src/main/java/com/sepism/pangu/model/repository/QuestionnaireAnswerRepository.java
+++ src/main/java/com/sepism/pangu/model/repository/QuestionnaireAnswerRepository.java
@@ -1,13 +1,21 @@
 package com.sepism.pangu.model.repository;
 
 import com.sepism.pangu.model.answer.QuestionnaireAnswer;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Transactional(readOnly = true)
-public interface QuestionnaireAnswerRepository extends JpaRepository<QuestionnaireAnswer, Long> {
+public interface QuestionnaireAnswerRepository extends JpaRepository<QuestionnaireAnswer, Long>,
+        PagingAndSortingRepository<QuestionnaireAnswer, Long> {
     List<QuestionnaireAnswer> findByQuestionnaireIdAndUserIdAndCurrent(long questionnaireId, long userId,
                                                                        boolean current);
+
+    Page<QuestionnaireAnswer> findByQuestionnaireIdAndCurrent(long questionnaireId, boolean current, Pageable page);
+
+    Page<QuestionnaireAnswer> findByCurrentIsTrue(Pageable page);
 }
